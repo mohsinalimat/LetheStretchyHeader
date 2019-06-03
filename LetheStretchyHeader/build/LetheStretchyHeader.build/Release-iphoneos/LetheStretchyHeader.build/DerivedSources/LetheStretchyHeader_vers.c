5f506b6def2bbfdb7b24fca814407d7489dc3aec
Osman <YILDIRIM>
2019-06-04 01:43:46 +0300

    v1.0.1

--- LetheStretchyHeader/build/LetheStretchyHeader.build/Release-iphoneos/LetheStretchyHeader.build/DerivedSources/LetheStretchyHeader_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char LetheStretchyHeaderVersionString[];
- extern const double LetheStretchyHeaderVersionNumber;
-
- const unsigned char LetheStretchyHeaderVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:LetheStretchyHeader  PROJECT:LetheStretchyHeader-1" "\n";
- const double LetheStretchyHeaderVersionNumber __attribute__ ((used)) = (double)1.;
