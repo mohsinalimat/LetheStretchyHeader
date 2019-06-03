61d7d85edd334023ec28b1bd8c1ccd505cbe4451
Osman <YILDIRIM>
2019-06-04 01:37:27 +0300

    v1.0.1

--- LetheStretchyHeader/build/LetheStretchyHeader.build/Release-iphoneos/LetheStretchyHeader.build/Objects-normal/arm64/LetheStretchyHeader_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Vizyoneks/LetheStretchyHeader/LetheStretchyHeader/build/LetheStretchyHeader.build/Release-iphoneos/LetheStretchyHeader.build/DerivedSources/LetheStretchyHeader_vers.c
